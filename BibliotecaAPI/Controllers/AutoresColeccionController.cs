db630f02d6727d8c86c951572d12fde29fec60d3
Alejandro Castañeda <alejandroxd62@gmail.com>
2025-09-15 17:56:29 -0600

    creacion del controlador para una coleccion de Autores, tambien se hizo el metodo post para crear varios autores al mismo tiempo (coleccion de autores)

---
+++ BibliotecaAPI/Controllers/AutoresColeccionController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BibliotecaAPI.Datos;
+using BibliotecaAPI.DTOs;
+using BibliotecaAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores-coleccion")]
+    public class AutoresColeccionController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public AutoresColeccionController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(IEnumerable<AutorCreacionDTO> autoresCreacionDTO)
+        {
+            var autores = mapper.Map<IEnumerable<Autor>>(autoresCreacionDTO);
+            context.AddRange(autores);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
