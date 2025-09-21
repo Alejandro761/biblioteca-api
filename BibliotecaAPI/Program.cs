dcd39eec1d8ce6b777c715fb9d9eaf2dbf207444
Alejandro Castañeda <alejandroxd62@gmail.com>
2025-09-20 21:41:22 -0600

    limpieza de proyecto: elimine archivos de condiguracion. tambien herede identitydbcontext en applicationDBContext

--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -5,28 +5,7 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var diccionarioConfiguraciones = new Dictionary<string, string>
-{
-    {"quien_soy", "un diccionario en memoria" }
-};
-
-builder.Configuration.AddInMemoryCollection(diccionarioConfiguraciones!);
-
 // Area de servicios
-
-builder.Services.AddOptions<PersonaOpciones>().
-    Bind(builder.Configuration.GetSection(PersonaOpciones.Seccion))
-    //las siguientes funciones validan las restricciones
-    //puestas en la clase PersonaOpciones
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-builder.Services.AddOptions<TarifaOpciones>().
-    Bind(builder.Configuration.GetSection(TarifaOpciones.Seccion))
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-builder.Services.AddSingleton<PagosProcesamiento>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
