18294a849c15ba2519722a43498ac51088313b31
Alejandro Castañeda <alejandroxd62@gmail.com>
2025-09-20 22:16:32 -0600

    configuracion de identity

--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -1,6 +1,10 @@
 using BibliotecaAPI;
 using BibliotecaAPI.Datos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +22,45 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 //configuramos ApplicationDbContext como un servicio
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => 
     opciones.UseSqlServer("name=DefaultConnection"));
+
+//identityUser es la clase que representa a un usuario
+//configuramos identity para que use ApplicationDbContext para
+//coenctarse con las tablas de usuarios en la bd
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+//UserManager es el manejador de usuarios que nos va a permitir
+//registrar usuarios validar contraseñas, etc
+builder.Services.AddScoped<UserManager<IdentityUser>>();
+//SignInManager nos permite autenticar usuarios
+builder.Services.AddScoped<SignInManager<IdentityUser>>();
+//nos permite acceder al contexto http desde cualquier clase
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthentication().AddJwtBearer(opciones =>
+{
+    //para que asp.netcore no cambie el numero de un claim por otro
+    //de manera automatica
+    opciones.MapInboundClaims = false;
+    opciones.TokenValidationParameters =
+    new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        //validar el tiempo de expiracion del token
+        ValidateLifetime = true,
+        //validar la llave secreta que se uso para firmar el token
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(
+                builder.Configuration["llavejwt"]!
+            )),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
