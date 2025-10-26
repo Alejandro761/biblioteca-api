a9799bc40a37a544ac34ff96f474f35c9a934390
Alejandro Castañeda <alejandroxd62@gmail.com>
2025-10-26 17:04:15 -0600

    filtro de acción que acepta parametros en cabecera

--- BibliotecaAPI/Controllers/AutoresController.cs
+++ BibliotecaAPI/Controllers/AutoresController.cs
@@ -18,6 +18,7 @@ namespace BibliotecaAPI.Controllers
     [ApiController]
     [Route("api/autores")]
     [Authorize(Policy = "esadmin")]
+    [FiltroAgregarCabeceras("controlador", "autores")]
     public class AutoresController : ControllerBase
     {
         //instancia de ApplicationDbContext para interactuar con la bd
@@ -45,6 +46,7 @@ namespace BibliotecaAPI.Controllers
         [AllowAnonymous]
         [OutputCache(Tags = [cache])]
         [ServiceFilter<MiFIltroDeAccion>()]
+        [FiltroAgregarCabeceras("accion", "obtener-autores")]
         public async Task<IEnumerable<AutorDTO>> Get([FromQuery] PaginacionDTO paginacionDTO)
         {
             var queryable = context.Autores.AsQueryable();
---
+++ BibliotecaAPI/Utilidades/FiltroAgregarCabecerasAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BibliotecaAPI.Utilidades
+{
+    // ActionFilterAttribute es un filtro de acción para aceptar argumentos
+    public class FiltroAgregarCabecerasAttribute: ActionFilterAttribute
+    {
+        private readonly string nombre;
+        private readonly string valor;
+
+        public FiltroAgregarCabecerasAttribute(string nombre, string valor)
+        {
+            this.nombre = nombre;
+            this.valor = valor;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            //antes de la ejecución de la acción
+            context.HttpContext.Response.Headers.Append(nombre, valor);
+            base.OnActionExecuting(context);
+            
+            //después de la ejecución de la acción
+        }
+    }
+}
