6e59e0d46487aa3d70ec90d79f3f7b443dba0afe
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-03-02 19:21:50 -0600

    Iniciando pruebas automaticas: prueba unitaria para PrimeraLetraMayuscula

---
+++ BibliotecaAPITests/PruebasUnitarias/Validaciones/PrimeraLetraMayusculaAttributePruebas.cs
@@ -0,0 +1,28 @@
+﻿using BibliotecaAPI.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaAPITests.PruebasUnitarias.Validaciones
+{
+    [TestClass]
+    public class PrimeraLetraMayusculaAttributePruebas
+    {
+        [TestMethod]
+        public void IsValid_RetornaExitoso_SiValueEsVacio() {
+            //Preparacion
+            var primeraLetraMayusculaAttribute = new PrimeraLetraMayusculaAttribute();
+            var validationContext = new ValidationContext(new object());
+            var value = string.Empty;
+
+            //Prueba
+            var resultado = primeraLetraMayusculaAttribute.GetValidationResult(value, validationContext);
+
+            //Verificacion
+            Assert.AreEqual(expected: ValidationResult.Success, actual: resultado);
+        }
+    }
+}
