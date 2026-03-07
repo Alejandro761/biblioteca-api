978e828c022fddea650833da543bdd345339d2d5
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-03-07 16:25:38 -0600

    probando un metodo get de libros controller que utiliza http context

---
+++ BibliotecaAPITests/PruebasUnitarias/Controllers/V1/LibrosControllerPruebas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaAPI.Controllers.V1;
+using BibliotecaAPI.DTOs;
+using BibliotecaAPITests.Utilidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace BibliotecaAPITests.PruebasUnitarias.Controllers.V1
+{
+    [TestClass]
+    public class LibrosControllerPruebas: BasePruebas
+    {
+        [TestMethod]
+        public async Task Get_RetornaCeroLibros_CuandoNoHayLibros()
+        {
+            // Preparacion
+            var nombreBD = Guid.NewGuid().ToString();
+            var context = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+            IOutputCacheStore outputCacheStore = null!;
+
+            var controller = new LibrosController(context, mapper, outputCacheStore);
+
+            // podemos crear un http context por defecto para probar metodos que utilicen http context
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var paginacionDTO = new PaginacionDTO(1, 1);
+
+            // Prueba
+
+            var respuesta = await controller.Get(paginacionDTO);
+
+            // Verificación
+            Assert.AreEqual(expected: 0, actual: respuesta.Count());
+        }
+    }
+}
