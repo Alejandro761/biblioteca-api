d7e0fbd6088b8043e0dfbdd5e78f6b6deae65135
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-03-18 01:19:00 -0600

    metodos auxiliares para crear usuarios en pruebas de integracion

--- BibliotecaAPITests/Utilidades/BasePruebas.cs
+++ BibliotecaAPITests/Utilidades/BasePruebas.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using AutoMapper;
 using BibliotecaAPI.Datos;
+using BibliotecaAPI.DTOs;
 using BibliotecaAPI.Utilidades;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +22,8 @@ namespace BibliotecaAPITests.Utilidades
     {
         // para que no haya problemas de minusculas y mayusculas a la hora de serializar un string
         protected readonly JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions {PropertyNameCaseInsensitive = true}; 
+
+        protected readonly Claim adminClaim = new Claim("esadmin", "1");
         
         protected ApplicationDbContext ConstruirContext (string nombreBD)
         {
@@ -69,6 +74,64 @@ namespace BibliotecaAPITests.Utilidades
             });
 
             return factory;
+        }
+
+        protected async Task<string> CrearUsuario (string nombreBD, WebApplicationFactory<Program> factory) 
+            => await CrearUsuario(nombreBD, factory, [], "ejemplo@email.com");
+        
+        protected async Task<string> CrearUsuario (string nombreBD, WebApplicationFactory<Program> factory, IEnumerable<Claim> claims) 
+            => await CrearUsuario(nombreBD, factory, claims, "ejemplo@email.com");
+
+        protected async Task<string> CrearUsuario (string nombreBD, WebApplicationFactory<Program> factory, IEnumerable<Claim> claims, string email)
+        {
+            var urlRegistro = "/api/v1/usuarios/registro";
+            string token = string.Empty;
+            token = await ObtenerToken(email, urlRegistro, factory);
+
+            if (claims.Any())
+            {
+                var context = ConstruirContext(nombreBD);
+                var usuario = await context.Users.Where(x => x.Email == email).FirstAsync();
+                Assert.IsNotNull(usuario);
+
+                // se mapean los claims al tipo IdentityUserClaim el cual acepta EF Core para permitirnos insertarlos en la tabla de claims del usuario
+                var userClaims = claims.Select(x => new IdentityUserClaim<string>
+                {
+                    UserId = usuario.Id,
+                    ClaimType = x.Type,
+                    ClaimValue = x.Value
+                });
+
+                context.UserClaims.AddRange(userClaims);
+                await context.SaveChangesAsync();
+
+                // hacemos el login del usuario para obtener un token que tenga los nuevos claims
+                var urlLogin = "/api/v1/usuarios/login";
+                token = await ObtenerToken(email, urlLogin, factory);
+            }
+
+            return token;
+        }
+
+        private async Task<string> ObtenerToken (string email, string url, WebApplicationFactory<Program> factory)
+        {
+            var password = "12345Aa!";
+            var credenciales = new CredencialesUsuariosDTO
+            {
+                Email = email, 
+                Password = password
+            };
+
+            var cliente = factory.CreateClient();
+            var respuesta = await cliente.PostAsJsonAsync(url, credenciales);
+            respuesta.EnsureSuccessStatusCode(); //asegurarnos que la respuesta sea exitosa
+
+            var contenido = await respuesta.Content.ReadAsStringAsync();
+            var respuestaAutenticación = JsonSerializer.Deserialize<RespuestaAutenticacionDTO>(contenido, jsonSerializerOptions)!;
+
+            Assert.IsNotNull(respuestaAutenticación.Token);
+
+            return respuestaAutenticación.Token;
         }
     }
 }
