239968adfb63367a70a37804ff3f68a89129208d
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-03-19 01:34:32 -0600

    probando que funcione el filtro de validacion de librosCreacionDTO en el Post de libros controller

---
+++ BibliotecaAPITests/PruebasDeIntegracion/Controllers/V1/LibrosControllerPruebas.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using BibliotecaAPI.DTOs;
+using BibliotecaAPITests.Utilidades;
+
+namespace BibliotecaAPITests.PruebasDeIntegracion.Controllers.V1
+{
+    [TestClass]
+    public class LibrosControllerPruebas: BasePruebas
+    {
+        private readonly string url = "/api/v1/libros";
+        private string nombreBD = Guid.NewGuid().ToString();
+
+        [TestMethod]
+        public async Task Post_Devuelve400_CuandoAutoresIdEsVacio ()
+        {
+            // Preparación
+            var factory = ConstruirWebApplicationFactory(nombreBD);
+            var cliente = factory.CreateClient();
+            var libroCreacionDTO = new LibroCreacionDTO {Titulo = "Ale's Journey"};
+
+            // Prueba
+            var respuesta = await cliente.PostAsJsonAsync(url, libroCreacionDTO);
+
+            // Verificación
+            Assert.AreEqual(expected: HttpStatusCode.BadRequest, actual: respuesta.StatusCode);
+        }
+    }
+}
