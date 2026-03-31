743a4707167e601f573e6aff57393108a90de396
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-03-31 01:58:40 -0600

    controller de llave y dtos necesarios

---
+++ BibliotecaAPI/DTOs/LlaveDTO.cs
@@ -0,0 +1,12 @@
+﻿using BibliotecaAPI.Entidades;
+
+namespace BibliotecaAPI.DTOs
+{
+    public class LlaveDTO
+    {
+        public int Id { get; set; }
+        public required string LLave { get; set; }
+        public bool Activa { get; set; }
+        public required string TipoLlave { get; set; }
+    }
+}
