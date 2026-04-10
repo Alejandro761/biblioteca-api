302251a6cf3cf95ca7d8a57ff81518c99a4e7232
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-04-09 21:53:44 -0600

    creacion de la entidad RestriccionDominio

---
+++ BibliotecaAPI/Entidades/RestriccionDominio.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaAPI.Entidades
+{
+    public class RestriccionDominio
+    {
+        public int Id { get; set; }
+        public int LlaveId { get; set; }
+        public required string Domininio { get; set; }
+        public LlaveAPI? Llave { get; set; }
+    }
+}
