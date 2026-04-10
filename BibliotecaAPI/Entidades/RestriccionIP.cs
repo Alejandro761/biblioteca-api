ad2bfbeb010d58f52c8b7fdc83c6547b5a361aed
Alejandro Castañeda <alejandroxd62@gmail.com>
2026-04-09 21:55:14 -0600

    creacion de la clase restriccionIP

---
+++ BibliotecaAPI/Entidades/RestriccionIP.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaAPI.Entidades
+{
+    public class RestriccionIP
+    {
+        public int Id { get; set; }
+        public int LlaveId { get; set; }
+        public required string IP { get; set; }
+        public LlaveAPI? Llave { get; set; }
+    }
+}
